5acf98b56225593fe2d047710abb72bba94f7b1b
hassibmoddasser <hassib.moddasser.cs@gmail.com>
2020-08-20 22:27:45 +0430

    Style of 'login' and 'register' pages is changed

--- login.php
+++ login.php
@@ -4,64 +4,68 @@ session_start();
 require_once 'inc/dbh.php';
 
 if (isset($_POST['submit'])) {
-    $user_name = filter(trim($_POST['user_name']));
-    $password = filter($_POST['password']);
+  $user_name = filter(trim($_POST['user_name']));
+  $password = filter($_POST['password']);
 
-    $result = mysqli_query($con, "SELECT * FROM `users` WHERE `user_name` = '$user_name'");
-    if (mysqli_num_rows($result) == 1) {
-        $user = mysqli_fetch_assoc($result);
-        
-        if (password_verify($password, $user['password'])) {
-            $_SESSION['full_name'] = $user['first_name'] . ' ' . $user['last_name'];
-            $_SESSION['login_status'] = true;
-            header('Location: ' . URL_ROOT . 'index.php');
-        } else {
-            header('Location: ' . URL_ROOT . 'login.php?err=true');
-        }
+  $result = mysqli_query($con, "SELECT * FROM `users` WHERE `user_name` = '$user_name'");
+  if (mysqli_num_rows($result) == 1) {
+    $user = mysqli_fetch_assoc($result);
+    
+    if (password_verify($password, $user['password'])) {
+      $_SESSION['full_name'] = $user['first_name'] . ' ' . $user['last_name'];
+      $_SESSION['login_status'] = true;
+      header('Location: ' . URL_ROOT . 'index.php');
+    } else {
+      header('Location: ' . URL_ROOT . 'login.php?err=true');
     }
+  }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Login</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Your Company | Login</title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/login.css">
-    </head>
-    <body>
-        <div class="container login-container">
-            <?php if (isset($_GET['err'])): ?>
-                <div class="row">
-                    <div class="col-md-5 m-auto text-center">
-                        <div class="alert alert-danger">Incorrect Username or Password! Please try again!</div>
-                    </div>
-                </div>
-            <?php endif; ?>
-            <div class="row">
-                <div class="col-md-4 m-auto login-form">
-                    <h3 class="text-center text-white mb-4">Login Form</h3>
-                    <form action="login.php" method="POST" autocomplete="false">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="user_name" placeholder="Username" required autofocus />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" name="password" placeholder="Password" required />
-                        </div>
-                        <div class="form-group mt-4">
-                            <input type="submit" class="btn btn-primary btn-login" name="submit" value="Login" />
-                            <a href="register.php">
-                                <button type="button" class="btn btn-success btn-register float-right">Don't have account?</button>
-                            </a>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+  <div class="container login-container">
+    <?php if (isset($_GET['err'])): ?>
+      <div class="row">
+        <div class="col-md-5 m-auto text-center">
+          <div class="alert alert-danger">Incorrect Username or Password! Please try again!</div>
         </div>
+      </div>
+    <?php endif; ?>
 
-        <link rel="stylesheet" href="js/bootstrap.min.js">
-    </body>
+    <!-- Your Company Logo -->
+    <div class="text-center mb-3">
+      <a href="#"><img src="img/logo.png" alt="logo" class="w-25"></a>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 m-auto login-form">
+        <form action="login.php" method="POST" autocomplete="false">
+          <div class="form-group">
+            <input type="text" class="form-control" name="user_name" placeholder="Username" required autofocus>
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Password" required>
+            <span class="text-muted password-info">Make sure you do not save your password in browser due to security reasons.</span>
+          </div>
+          <div class="form-group text-right mt-4 mb-0">
+            <input type="submit" class="btn btn-primary btn-block mb-2 btn-login" name="submit" value="Sign In">
+            <a href="register.php" class="text-muted register-link">Don't have account?</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <script rel="stylesheet" href="js/bootstrap.min.js"></script>
+</body>
 </html>
--- register.php
+++ register.php
@@ -2,58 +2,61 @@
 require_once 'inc/dbh.php';
 
 if (isset($_POST['submit'])) {
-    $first_name = filter(trim($_POST['first_name']));
-    $last_name = filter(trim($_POST['last_name']));
-    $user_name = filter(trim($_POST['user_name']));
-    $password = password_hash(filter($_POST['password']), PASSWORD_BCRYPT);
+  $first_name = filter(trim($_POST['first_name']));
+  $last_name = filter(trim($_POST['last_name']));
+  $user_name = filter(trim($_POST['user_name']));
+  $password = password_hash(filter($_POST['password']), PASSWORD_BCRYPT);
 
-    $query = "INSERT INTO `users`(`first_name`, `last_name`, `user_name`, `password`) VALUES('$first_name', '$last_name', '$user_name', '$password')";
-    $result = @mysqli_query($con, $query);
-    if ($result) {
-        header('Location: ' . URL_ROOT . 'login.php');
-    }
+  $query = "INSERT INTO `users`(`first_name`, `last_name`, `user_name`, `password`) VALUES('$first_name', '$last_name', '$user_name', '$password')";
+  $result = @mysqli_query($con, $query);
+  if ($result) {
+    header('Location: ' . URL_ROOT . 'login.php');
+  }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Register</title>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Your Company | Register</title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/login.css">
-    </head>
-    <body>
-        <div class="container login-container">
-            <div class="row">
-                <div class="col-md-4 m-auto login-form">
-                    <h3 class="text-center text-white mb-4">Registration Form</h3>
-                    <form action="register.php" method="POST" autocomplete="false">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="First name" name="first_name" minlength="3" autofocus required />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Last name" name="last_name" minlength="3" required />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Username" name="user_name" minlength="8" required />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Password" name="password" minlength="8" required />
-                        </div>
-                        <div class="form-group mt-4">
-                            <input type="submit" class="btn btn-primary btn-login" name="submit" value="Register" />
-                            <a href="login.php">
-                                <button type="button" class="btn btn-success btn-register float-right">Wanna login?</button>
-                            </a>
-                        </div>
-                    </form>
-                </div>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+  </head>
+  <body>
+    <div class="container register-container">
+
+      <!-- Your Company Logo -->
+      <div class="text-center mb-3">
+        <a href="#"><img src="img/logo.png" alt="logo" class="w-25"></a>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4 m-auto register-form">
+          <form action="register.php" method="POST" autocomplete="false">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="First name" name="first_name" minlength="3" autofocus required>
             </div>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Last name" name="last_name" minlength="3" required>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Username" name="user_name" minlength="8" required>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="Password" name="password" minlength="8" required>
+            </div>
+            <div class="form-group text-right mt-4 mb-0">
+              <input type="submit" class="btn btn-primary btn-block mb-2 btn-login" name="submit" value="Sign Up">
+              <a href="login.php" class="text-muted register-link">I do have an account!</a>
+            </div>
+          </form>
         </div>
+      </div>
+    </div>
 
-        <link rel="stylesheet" href="js/bootstrap.min.js">
-    </body>
+    <script rel="stylesheet" href="js/bootstrap.min.js"></script>
+  </body>
 </html>
