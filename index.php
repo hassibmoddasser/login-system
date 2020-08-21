3c614ada88129b0b76b8c38b382b85a64787b9e1
hassibmoddasser <hassib.moddasser.cs@gmail.com>
2020-08-21 10:32:01 +0430

    Custom btn style added in 'app.css'

--- index.php
+++ index.php
@@ -7,11 +7,10 @@ include_once 'inc/nav.php'
   <div class="row">
     <div class="col-md-12 text-center mt-3">
       <div class="jumbotron">
-        <h1>Welcome to a <?= APP_NAME ?></h1>
+        <h1 class="display-4">Welcome to a <?= APP_NAME ?></h1>
         <p class="lead">Thank you so much for visiting!</p>
         <div class="btn-group">
-          <a href="<?= URL_ROOT . 'users.php' ?>" class="btn btn-primary btn-lg" role="button">Registered Users</a>
-          <a href="<?= URL_ROOT . 'register.php' ?>" class="btn btn-success btn-lg">Register</a>
+          <a href="<?= URL_ROOT . 'users.php' ?>" class="btn btn-primary custom-btn" role="button">Registered Users</a>
         </div>
       </div>
     </div>
